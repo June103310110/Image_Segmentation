06542c6e9fd5c3d1baed4fcbad0ca851a0834627
June Wang <june103310110@gmail.com>
2022-05-03 14:37:55 +0800

    update set.py

--- 05_Datasets/CHAOS_liver_segmentation/set.py
+++ 05_Datasets/CHAOS_liver_segmentation/set.py
@@ -1,5 +1,7 @@
 import os 
 os.system('wget https://github.com/June103310110/Image_Segmentation/releases/download/datasets/requirements_chaos.txt')
+os.system('wget https://raw.githubusercontent.com/June103310110/Image_Segmentation/main/03_Models/loss.py')
+os.system('wget https://raw.githubusercontent.com/June103310110/Image_Segmentation/main/03_Models/unet.py')
 os.system('pip3 install -q -r ./requirements_chaos.txt')
 os.system('pip3 install --upgrade gdown')
 
--- 05_Datasets/MVTec_anomaly_detection/set.py
+++ 05_Datasets/MVTec_anomaly_detection/set.py
@@ -1,5 +1,7 @@
 import os 
 os.system('wget https://github.com/June103310110/Image_Segmentation/releases/download/datasets/requirements_mvtec.txt')
+os.system('wget https://raw.githubusercontent.com/June103310110/Image_Segmentation/main/03_Models/loss.py')
+os.system('wget https://raw.githubusercontent.com/June103310110/Image_Segmentation/main/03_Models/unet.py')
 os.system('pip3 install -r ./requirements_mvtec.txt')
 os.system('pip3 install -U gdown')
 import gdown 
